54857e1a8f7a1ee22fd57c560294e9f711a5394f
313shazid313 <hossainshazid567@gmail.com>
2024-09-04 10:06:15 +0600

    added admin panel

--- frontend/src/Admin/AdminLogin.jsx
+++ frontend/src/Admin/AdminLogin.jsx
@@ -1,11 +1,20 @@
-
-
 const AdminLogin = () => {
   return (
     <>
-      
+      <div className="input-group flex-nowrap">
+        <span className="input-group-text" id="addon-wrapping">
+          @
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="addon-wrapping"
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AdminLogin
+export default AdminLogin;
