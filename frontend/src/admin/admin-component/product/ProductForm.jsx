c021a68e950f66b1054fd48f4b628a7184c8ccc7
313shazid313 <hossainshazid567@gmail.com>
2025-01-07 00:04:22 +0600

    added some cool features

--- frontend/src/admin/admin-component/product/ProductForm.jsx
+++ frontend/src/admin/admin-component/product/ProductForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import ImageHandle from "../../admin-component/ImageHandle";
 import {
   useCreateProductMutation,
   useGetAllProductsQuery,
@@ -16,6 +17,9 @@ import { useGetAllBrandsQuery } from "../../../redux/product-additional-state/br
 import Select from "react-select";
 
 const ProductForm = () => {
+  const [corporateForm, setCorporateForm] = useState(false);
+  const [image, setImage] = useState("");
+
   const { refetch } = useGetAllProductsQuery();
   const [createProduct] = useCreateProductMutation();
   const navigate = useNavigate();
@@ -40,18 +44,20 @@ const ProductForm = () => {
     name: "",
     description: "",
     specification: "",
-    imageurl: "",
     price: "",
-    mrpPrice: "",
-    expiryDate: "",
-    sellType: "",
+    deletePrice: "",
+    imageURL: "",
+    offer: "",
+    status: "",
+    isPublished: "",
     vat: "",
-    quantity: "",
-    ProductCategoryId: "",
-    ProductOriginId: "",
-    ProductBrandId: "",
-    ProductTypeId: "",
-    ProductUnitId: "",
+    sellType: "",
+    preOrder: "",
+    categoryId: "",
+    typeId: "",
+    brandId: "",
+    originId: "",
+    unitId: "",
   });
 
   const handleInputChange = (e) => {
@@ -60,6 +66,10 @@ const ProductForm = () => {
       ...items,
       [name]: value,
     });
+
+    if (e.target.value === "corporate-sell") {
+      setCorporateForm(true); // Example logic if using React state
+    }
   };
 
   const handleProductSubmit = async (event) => {
@@ -83,11 +93,17 @@ const ProductForm = () => {
     (categories || []).forEach((category) => {
       // Build the current path
       const currentPath = parentPath
-        ? `${parentPath} => ${category.categoryName}`
-        : category.categoryName;
+        ? `${parentPath} => ${category.name}`
+        : category.name;
 
       // Add the current path as an object with name and id
-      paths.push({ name: currentPath, id: category.id });
+      paths.push({
+        name: currentPath,
+        id: category.id,
+        isPublished: category.isPublished,
+        inHomeCategory: category.inHomeCategory,
+        imageURL: category.imageURL,
+      });
 
       // Recursively process children
       const childPaths = buildParentChildPaths(
@@ -113,7 +129,7 @@ const ProductForm = () => {
       value: item.id,
     })),
   ];
-  // console.log(optionsArray);
+  console.log(optionsArray);
   //? organizing category data -------->
 
   return (
@@ -166,13 +182,13 @@ const ProductForm = () => {
                 htmlFor="last-name"
                 className="block text-sm/6 font-medium text-gray-900"
               >
-                MRP Price
+                Delete Price
               </label>
               <div className="mt-2">
                 <input
                   id="last-name"
-                  name="mrpPrice"
-                  value={items.mrpPrice}
+                  name="deletePrice"
+                  value={items.deletePrice}
                   type="number"
                   autoComplete="family-name"
                   onChange={handleInputChange}
@@ -208,24 +224,6 @@ const ProductForm = () => {
                 Product Category
               </label>
               <div className="mt-2">
-                {/* <select
-                  id="ProductCategoryId"
-                  name="ProductCategoryId"
-                  value={items.ProductCategoryId || ""}
-                  onChange={handleInputChange}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600-600 sm:text-sm/6"
-                >
-                  <option value="" disabled>
-                    -----Select Category-----
-                  </option>
-                  {categoryData &&
-                    categoryData.map((Item) => (
-                      <option key={Item.id} value={Item.id}>
-                        {Item.categoryName}
-                      </option>
-                    ))}
-                </select> */}
-
                 <Select
                   options={optionsArray}
                   // defaultValue={optionsArray[1]}
@@ -268,7 +266,7 @@ const ProductForm = () => {
                   {brandData &&
                     brandData.map((Item) => (
                       <option key={Item._id} value={Item._id}>
-                        {Item.brandName}
+                        {Item.name}
                       </option>
                     ))}
                 </select>
@@ -296,7 +294,7 @@ const ProductForm = () => {
                   {typeData &&
                     typeData.map((Item) => (
                       <option key={Item._id} value={Item._id}>
-                        {Item.TypeName}
+                        {Item.name}
                       </option>
                     ))}
                 </select>
@@ -324,7 +322,7 @@ const ProductForm = () => {
                   {originData &&
                     originData.map((Item) => (
                       <option key={Item._id} value={Item._id}>
-                        {Item.originName}
+                        {Item.name}
                       </option>
                     ))}
                 </select>
@@ -352,7 +350,7 @@ const ProductForm = () => {
                   {unitData &&
                     unitData.map((Item) => (
                       <option key={Item._id} value={Item._id}>
-                        {Item.UnitName}
+                        {Item.name}
                       </option>
                     ))}
                 </select>
@@ -370,7 +368,11 @@ const ProductForm = () => {
               Description
             </label>
             <div>
-              <JoditEditor />
+              <JoditEditor
+                name="description"
+                value={items.description}
+                onChange={handleInputChange}
+              />
             </div>
           </div>
 
@@ -382,9 +384,25 @@ const ProductForm = () => {
               Specification
             </label>
             <div>
-              <JoditEditor />
+              <JoditEditor
+                name="specification"
+                value={items.specification}
+                onChange={handleInputChange}
+              />
             </div>
           </div>
+        </div>
+
+        {/*photos  */}
+        <div className="flex flex-col space-y-1 my-3">
+          <ImageHandle
+            label="Image"
+            name="image"
+            id="image"
+            value={image}
+            placeholder="Upload image"
+            setImage={setImage}
+          />
         </div>
 
         <div className="border-gray-900/10">
@@ -471,64 +489,10 @@ const ProductForm = () => {
               </div>
             </fieldset>
           </div>
+          {corporateForm ? <p>asdasdasddasdasdasd</p> : <></>}
         </div>
       </div>
 
-      {/*photos  */}
-      <div className="col-span-full py-6">
-        <label className="block text-sm/6 font-medium text-gray-900">
-          Product Image
-        </label>
-        <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-          <div className="text-center">
-            <svg
-              className="mx-auto size-12 text-gray-300"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              aria-hidden="true"
-              data-slot="icon"
-            >
-              <path
-                fillRule="evenodd"
-                d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <div className="mt-4 flex text-sm/6 text-gray-600">
-              <label className="relative cursor-pointer rounded-md bg-white font-semibold text-blring-blue-600-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-blue-600-600 focus-within:ring-offset-2 hover:text-blring-blue-600-500">
-                <span>Upload a file</span>
-                <input
-                  id="file-upload"
-                  name="file-upload"
-                  type="file"
-                  className="sr-only"
-                />
-              </label>
-              <p className="pl-1">or drag and drop</p>
-            </div>
-            <p className="text-xs/5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-          </div>
-        </div>
-      </div>
-      {/* date */}
-      <div className="py-6 max-w-64">
-        <label
-          htmlFor="email"
-          className="block text-sm/6 font-medium text-gray-900"
-        >
-          Expiry Date
-        </label>
-        <div className="mt-2">
-          <input
-            id="expiryDate"
-            name="expiryDate"
-            value={items.expiryDate}
-            onChange={handleInputChange}
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600-600 sm:text-sm/6"
-            type="date"
-          />
-        </div>
-      </div>
       {/* submit button */}
       <div className="mt-6 flex items-center justify-end gap-x-6">
         <button
